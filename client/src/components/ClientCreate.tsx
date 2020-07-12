933bb738334fb421147077211b26703d0b276a81
Warpfox <tylermwatts@gmail.com>
2020-07-12 12:21:38 -0500

    extract Form as a component, refactor accordingly

--- client/src/components/ClientCreate.tsx
+++ client/src/components/ClientCreate.tsx
@@ -1,10 +1,10 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { ErrorMessage } from "@hookform/error-message";
 import React from "react";
 import { useForm } from "react-hook-form";
+import Form, { LabeledFormInput } from "./Form";
 
-const container = css`
+const page = css`
   text-align: center;
 `;
 
@@ -13,53 +13,9 @@ const formContainer = css`
   justify-content: center;
 `;
 
-const form = css`
-  max-width: 300px;
-  text-align: left;
-`;
-
-const input = css`
-  display: block;
-  padding: 5px;
-  border: 2px solid #616161;
-  border-radius: 5px;
-  margin: 10px 0;
-`;
-
-const errorStyle = css`
-  display: block;
-  color: red;
-`;
-
-const formButton = css`
-  display: block;
-  background-color: #004cff;
-  color: #d3d3d3;
-  border: 2px solid #616161;
-  border-radius: 5px;
-  padding: 5px;
-  margin: 20px 0;
-  width: 100%;
-  &:disabled {
-    background-color: lightgray;
-    color: gray;
-  }
-`;
-
 const statesRegex = /^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/;
 
 const ClientCreate: React.FC<{}> = (props) => {
-  return (
-    <div css={container}>
-      <h1>Create New Organization</h1>
-      <div>
-        <ClientCreateForm />
-      </div>
-    </div>
-  );
-};
-
-const ClientCreateForm: React.FC<{}> = (props) => {
   const { errors, formState, handleSubmit, register, reset } = useForm({
     mode: "onChange",
   });
@@ -69,77 +25,53 @@ const ClientCreateForm: React.FC<{}> = (props) => {
   };
   const { isValid } = formState;
   return (
-    <div css={formContainer}>
-      <form
-        css={form}
-        id="client-create-form"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <LabeledFormInput
-          type="text"
-          name="organization"
-          labelText="Organization"
-          register={register({ required: "Organization name is required." })}
-          errors={errors}
-        />
-        <LabeledFormInput
-          type="text"
-          name="city"
-          labelText="City"
-          register={register({ required: "City is required." })}
-          errors={errors}
-        />
-        <LabeledFormInput
-          type="text"
-          name="state"
-          labelText="State"
-          register={register({
-            required: "State is required.",
-            pattern: {
-              value: statesRegex,
-              message: "Must be a valid 2-digit state abbreviation.",
-            },
-          })}
-          maxLength={2}
-          errors={errors}
-        />
-        <LabeledFormInput
-          type="number"
-          name="zip"
-          labelText="ZIP Code"
-          register={register({
-            required: "ZIP Code is required.",
-            pattern: {
-              value: /\d{5}/g,
-              message: "ZIP Code must be exactly 5 numerical digits.",
-            },
-          })}
-          errors={errors}
-        />
-        <button disabled={!isValid} css={formButton} form="client-create-form">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
-
-interface LabeledFormInputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  type: string;
-  name: string;
-  labelText: string;
-  register: any;
-  errors: any;
-}
-
-const LabeledFormInput = (props: LabeledFormInputProps) => {
-  const { type, name, labelText, register, errors, ...rest } = props;
-  return (
-    <div>
-      <label htmlFor={name}>{labelText}</label>
-      <input css={input} type={type} name={name} ref={register} {...rest} />
-      <ErrorMessage css={errorStyle} as="p" errors={errors} name={name} />
+    <div css={page}>
+      <h1>Create New Organization</h1>
+      <div css={formContainer}>
+        <Form onSubmit={handleSubmit(onSubmit)} isValid={isValid}>
+          <LabeledFormInput
+            type="text"
+            name="organization"
+            labelText="Organization"
+            register={register({ required: "Organization name is required." })}
+            errors={errors}
+          />
+          <LabeledFormInput
+            type="text"
+            name="city"
+            labelText="City"
+            register={register({ required: "City is required." })}
+            errors={errors}
+          />
+          <LabeledFormInput
+            type="text"
+            name="state"
+            labelText="State"
+            register={register({
+              required: "State is required.",
+              pattern: {
+                value: statesRegex,
+                message: "Must be a valid 2-digit state abbreviation.",
+              },
+            })}
+            maxLength={2}
+            errors={errors}
+          />
+          <LabeledFormInput
+            type="number"
+            name="zip"
+            labelText="ZIP Code"
+            register={register({
+              required: "ZIP Code is required.",
+              pattern: {
+                value: /\d{5}/g,
+                message: "ZIP Code must be exactly 5 numerical digits.",
+              },
+            })}
+            errors={errors}
+          />
+        </Form>
+      </div>
     </div>
   );
 };
---
+++ client/src/components/Form.tsx
@@ -0,0 +1,76 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import { ErrorMessage } from "@hookform/error-message";
+import * as React from "react";
+
+const form = css`
+  max-width: 300px;
+  text-align: left;
+`;
+
+const formButton = css`
+  display: block;
+  background-color: #004cff;
+  color: #d3d3d3;
+  border: 2px solid #616161;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 20px 0;
+  width: 100%;
+  &:disabled {
+    background-color: lightgray;
+    color: gray;
+  }
+`;
+
+const input = css`
+  display: block;
+  padding: 5px;
+  border: 2px solid #616161;
+  border-radius: 5px;
+  margin: 10px 0;
+`;
+
+const errorStyle = css`
+  display: block;
+  color: red;
+`;
+
+export interface FormProps extends React.ComponentProps<"form"> {
+  isValid: boolean;
+  onSubmit: any;
+}
+
+const Form = (props: FormProps) => {
+  const { isValid, onSubmit, children, ...rest } = props;
+  return (
+    <form css={form} onSubmit={onSubmit} {...rest}>
+      {children}
+      <button disabled={!isValid} css={formButton} type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+interface LabeledFormInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  type: string;
+  name: string;
+  labelText: string;
+  register: any;
+  errors: any;
+}
+
+export const LabeledFormInput = (props: LabeledFormInputProps) => {
+  const { type, name, labelText, register, errors, ...rest } = props;
+  return (
+    <div>
+      <label htmlFor={name}>{labelText}</label>
+      <input css={input} type={type} name={name} ref={register} {...rest} />
+      <ErrorMessage css={errorStyle} as="p" errors={errors} name={name} />
+    </div>
+  );
+};
+
+export default Form;
